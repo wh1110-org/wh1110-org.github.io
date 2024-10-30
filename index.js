d2cdd17b34f2832b4272aac6c023a53f19e4afed
matcha1024 <pettyblue@proton.me>
2024-10-30 23:08:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ get_quiz_data();
 reg_start_event();
 
 // Page Generations
-function gen_top_content(){
+function gen_top_content() {
 	var ins = '<h1 class="p-quiz-title">世界史1110本ノック</h1>';
 	ins += '<h3 class="p-quiz-subtitle">センター過去問とその解説</h3>';
 	ins += '<div class="p-quiz-next">';
@@ -18,12 +18,12 @@ function gen_top_content(){
 	document.querySelector('.js-quiz-content').innerHTML = ins;
 }
 
-function gen_quiz_content(){
-	var ins = '<h1 class="p-quiz-title">' + quiz_data[current_no]["q"] + '</h1>';
+function gen_quiz_content() {
+	var ins = '<h1 class="p-quiz-title">' + quiz_data["quiz"][current_no]["q"] + '</h1>';
 	ins += '<ol class="p-quiz-choices">';
-	for(var i=0; i<quiz_data["quiz"][current_no]["a"].length; i++){
+	for (var i = 0; i < quiz_data["quiz"][current_no]["a"].length; i++) {
 		ins += '<li class="p-quiz-choices_item>';
-		ins += '	<button class="c-btn js-quiz-choice" data-quiz_choice="' + (i+1) + '">' + quiz_data["quiz"][current_no]["a"][i] + '</button>';
+		ins += '	<button class="c-btn js-quiz-choice" data-quiz_choice="' + (i + 1) + '">' + quiz_data["quiz"][current_no]["a"][i] + '</button>';
 		ins += '</li>';
 	}
 	ins += '</ol>';
@@ -33,9 +33,9 @@ function gen_quiz_content(){
 
 
 // Get Quiz Data
-function get_quiz_data(){
+function get_quiz_data() {
 	let xhr = new XMLHttpRequest();
-	xhr.onload = function(){
+	xhr.onload = function () {
 		quiz_data = xhr.response;
 	}
 	xhr.open("GET", "quiz.json");
@@ -45,8 +45,8 @@ function get_quiz_data(){
 
 
 // Event Registrations
-function reg_start_event(){
-	document.querySelector(".js-quiz-start").addEventListener("click", function(){
+function reg_start_event() {
+	document.querySelector(".js-quiz-start").addEventListener("click", function () {
 		gen_quiz_content();
 	}, false);
 }
